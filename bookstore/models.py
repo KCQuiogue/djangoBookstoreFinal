bd2b3e3a806981c1fca2b2b751efc0ea4dc9e6a2
Jeanne-Tan-120 <84549604+Jeanne-Tan-120@users.noreply.github.com>
2023-12-11 23:05:44 +0800

    Update models.py

--- bookstore/models.py
+++ bookstore/models.py
@@ -38,3 +38,24 @@ class CartItem(models.Model):
 
     def __str__(self):
         return f"{self.quantity} of {self.book.title}"
+
+# Ordering
+class Order(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    is_paid = models.BooleanField(default=False)
+    total_price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return f"Order {self.id}"
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, related_name='items', on_delete=models.CASCADE)
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    quantity = models.IntegerField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+
+    def __str__(self):
+        return f"{self.quantity} of {self.book.title}"
+
